00c9bd6f22f31d3c3ddca417fe8876b3602774f6
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-07-21 20:52:56 -0300

    Add storage functionality.

--- script.js
+++ script.js
@@ -17,3 +17,36 @@ btn2.onclick = () => {
 	count = 0;
 	num.innerText = count;
 }
+
+const todoList = document.getElementById("todo");
+
+todoList.innerText = "Nothing to do.";
+
+const STORAGE_KEY = "shit-to-do";
+
+function addSomeShit(someShit) {
+	const shitToDo = getAllTheShit();
+
+	shitToDo.push(someShit);
+
+	window.localStorage.setItem(STORAGE_KEY, JSON.stringify(shitToDo));
+}
+
+function getAllTheShit() {
+	const data = window.localStorage.getItem(STORAGE_KEY);
+
+	const shitToDo = data ? JSON.parse(data) : [];
+
+	return shitToDo;
+}
+
+const userInput = document.getElementById("user-input");
+
+userInput.onkeydown = (e) => {
+	if (e.key === "Enter" && userInput.value !== "") {
+		let newItem = userInput.value;
+		userInput.value = "";
+		console.log(`Item: ${newItem}`);
+		addSomeShit(newItem);
+	}
+}
